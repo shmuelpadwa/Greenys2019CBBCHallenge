cd8c8dd58d8daac37621adc97cffd2c69a68c572
Shmu <padwas@bxscience.edu>
2019-03-18 20:09:31 -0400

    Create Team.java

---
+++ Team.java
@@ -0,0 +1,17 @@
+public class Team{
+  int seed;
+  String teamName;
+  double r32, r16, r8, r4, r2, r1;
+
+  public Team(int seed, String teamName, double r32, double r16, double r8, double r4, double r2, double r1){
+    this.seed = seed;
+    this.teamName = teamName;
+    this.r32 = r32;
+    this.r16 = r16;
+    this.r8 = r8;
+    this.r4 = r4;
+    this.r2 = r2;
+    this.r1 = r1;
+  }
+
+}
